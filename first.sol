590ef98466a6e3b4641fd8a72c164efaf58d21e4
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-09-12 19:21:46 +0530

    Initial commit

---
+++ first.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    //boolean, uint, int, address, bytes
+    bool hasFavoriteNumber = true;
+    uint number =123; //size 256bits
+    uint256 n=123;
+    string FavNo= "Five"; 
+    int256 favoriteNo= -5;
+    address myAdd= 0x5228cf0BD4fd18d5a60dE8ed86b6180fae18D617;
+    bytes32 favBytes= "CAT";
+
+
+}
